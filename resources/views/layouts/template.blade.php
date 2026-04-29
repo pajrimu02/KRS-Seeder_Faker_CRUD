ec7cec4789044f92b092f3fb4bd72c280de23c1c
pajimu02 <pajrimuhamad751@gmail.com>
2026-04-29 18:09:14 +0700

    Tambah efek pada navbar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -18,12 +18,16 @@
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto">
 
-                <li class="nav-item">
-                    <a class="nav-link fw-bold text-white" href="/">Beranda</a>
+               <li class="nav-item">
+                    <a class="nav-link fw-bold text-white {{ request()->is('/') ? 'active-link' : '' }}" href="/">
+                        Beranda
+                    </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link fw-bold text-white" href="/tentang">Tentang</a>
+                    <a class="nav-link fw-bold text-white {{ request()->is('tentang') ? 'active-link' : '' }}" href="/tentang">
+                        Tentang
+                    </a>
                 </li>
 
                 <!-- DROPDOWN -->
@@ -33,11 +37,35 @@
                     </a>
 
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/dosen">Dosen</a></li>
-                        <li><a class="dropdown-item" href="/mahasiswa">Mahasiswa</a></li>
-                        <li><a class="dropdown-item" href="/matakuliah">Matakuliah</a></li>
-                        <li><a class="dropdown-item" href="/jadwal">Jadwal</a></li>
-                        <li><a class="dropdown-item" href="/krs">KRS</a></li>
+                        <li>
+                        <a class="dropdown-item {{ request()->is('dosen*') ? 'active-dropdown' : '' }}" href="/dosen">
+                            Dosen
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item {{ request()->is('mahasiswa*') ? 'active-dropdown' : '' }}" href="/mahasiswa">
+                            Mahasiswa
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item {{ request()->is('matakuliah*') ? 'active-dropdown' : '' }}" href="/matakuliah">
+                            Matakuliah
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item {{ request()->is('jadwal*') ? 'active-dropdown' : '' }}" href="/jadwal">
+                            Jadwal
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item {{ request()->is('krs*') ? 'active-dropdown' : '' }}" href="/krs">
+                            KRS
+                        </a>
+                    </li>
                     </ul>
                 </li>
 
@@ -61,4 +89,36 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+    <style>
+        .nav-link {
+    position: relative;
+    display: inline-block;
+}
+
+/* hanya untuk link biasa, bukan dropdown */
+.navbar .nav-link:not(.dropdown-toggle)::after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 0;
+    width: 0;
+    height: 2px;
+    background: white;
+    transition: 0.3s ease;
+}
+
+.navbar .nav-link:not(.dropdown-toggle):hover::after {
+    width: 50%;
+}
+
+.active-link::after {
+    width: 50%;
+}
+
+.active-dropdown {
+    background-color: #0d6efd;
+    color: white !important;
+}
+    </style>
 </html>
